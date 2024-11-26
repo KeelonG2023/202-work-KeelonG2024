1835849ff20d01ee6befa757c7f83724868dd068
Keelon <kkg2022@g.rit.edu>
2024-11-25 23:09:46 -0500

    Import Assets
    imported assets for Project 3.

---
+++ 2D Project/Assets/Scripts/Humanoid.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Humanoid : Entity
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        attraction = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D Project/Assets/Scripts/Tree.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : Entity
+{ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attraction = -0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
